17423a9332f29ee645f1b6fde912c38e82229829
jhonmalagon <jhon.malagon@flightaware.com>
2023-03-29 15:59:05 -0500

    Add development file

---
+++ dev.py
@@ -0,0 +1,7 @@
+import pywhatkit as pwk
+from globals import disciples, rides
+
+pwk.sendwhatmsg_instantly(
+    "+1 (832) 382-6123",
+    "Solo un recurdo de que hoy hay group a las 7:30pm!! Nos vemos pronto!",
+)
