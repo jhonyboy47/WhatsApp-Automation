30968db0b20c0f61b770e6a4eff9858cc6da3c04
jhonmalagon <jhon.malagon@flightaware.com>
2023-03-29 15:58:25 -0500

    Introduce global variables

---
+++ globals.py
@@ -0,0 +1,47 @@
+cesar = "+1 (832) 806-4381"
+ercy_esquivel = "+1 (346) 744-8609"
+jefry_zuniga = "+1 (575) 988-6194"
+oswaldo = "+58 414-1614969"
+rasheed = "+1 (832) 852-4503"
+santiago = "+1 (281) 515-7522"
+thomas_bohorquez = "+1 (832) 309-8429"
+disciples = [
+    cesar,
+    ercy_esquivel,
+    jefry_zuniga,
+    oswaldo,
+    rasheed,
+    santiago,
+    thomas_bohorquez,
+]
+
+leo_molina = "+1 (936) 217-6756"
+frank = "+1 (346) 946-1307"
+
+Eh = [leo_molina, frank]
+
+josue = "+1 (516) 419-8117"
+mario = "+1 (281) 406-5152"
+lalo = "+1 (936) 499-9984"
+jhonny = "+1 (832) 480-0052"
+close_friends = [
+    josue,
+    mario,
+    lalo,
+    jhonny,
+]
+
+iris = "+1 (346) 232-8802"
+rides = [jefry_zuniga, cesar, iris]
+
+# Worship {
+#    "Chris":
+#    "Dani Armas":
+#    "Sam Gomez":
+#    "Erick Lopez":
+#    "Jhonny":
+#    "Guadalupe":
+#    "Dariana":
+#    "Aariana":
+#    "Aziel":
+# }
