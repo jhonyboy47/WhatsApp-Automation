ce7092f7a33c2bc2d1a938e503787079530cb28a
jhonyboy47 <malagon@mines.edu>
2023-03-30 01:07:26 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -3,5 +3,5 @@ from globals import disciples, rides
 
 pwk.sendwhatmsg_instantly(
     "+1 (832) 382-6123",
-    "Solo un recurdo de que hoy hay group a las 7:30pm!! Nos vemos pronto!",
+    "Solo un recuerdo de que hoy hay group a las 7:30pm!! Nos vemos pronto!",
 )
